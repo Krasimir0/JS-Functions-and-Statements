cdc79f9498e95c73bfa5130813762e87abb27d65
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-07-03 16:10:42 +0100

    Finish exercise 2

--- 1.Smallest of Three Numbers.js
+++ 1.Smallest of Three Numbers.js
@@ -1,5 +1,6 @@
 function getTheSmallestOne(firstNum, secondNum, thirdNum) {
-    
+    let MinNUm = Math.min(firstNum, secondNum, thirdNum)
+    console.log(MinNUm);
 }
 
 getTheSmallestOne(600 ,342, 123)
