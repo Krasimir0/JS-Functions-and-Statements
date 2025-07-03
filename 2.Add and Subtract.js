beb42b0920b7bacb9e2852568f3034dccadc6b6d
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-07-03 16:19:41 +0100

    Add exercise 2

---
+++ 2.Add and Subtract.js
@@ -0,0 +1,10 @@
+function sum(firstNum, SecondNum) {
+    return firstNum + SecondNum
+}
+
+function subtract(firstNum, secondNum, thirdNum) {
+    let result = sum(firstNum, secondNum);
+    result - thirdNum;
+}
+
+subtract(23, 6, 10)
