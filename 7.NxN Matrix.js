a8f7c8c1eb8348468ecb67bb6f31aad8702d6f0d
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-07-06 15:11:28 +0100

    Add and Finish exercise 7

---
+++ 7.NxN Matrix.js
@@ -0,0 +1,11 @@
+function matrixNxN(num) {
+    for (let i = 0; i < num; i++) {
+        let row = '';
+        for (let j = 0; j < num; j++) {
+            row += num + ' '
+        }
+        console.log(row.trim());
+    }
+}
+
+matrixNxN(3)
