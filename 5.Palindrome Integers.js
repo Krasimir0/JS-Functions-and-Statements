9b9d4b5af019297d0d7e8c6c063c8411a655f9ca
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-07-06 14:56:52 +0100

    Finish exercise 5

--- 5.Palindrome Integers.js
+++ 5.Palindrome Integers.js
@@ -1,4 +1,18 @@
 function CheckIfPolindrome(arr) {
+    let arrOfStringNums = []
+    for (const number of arr) {
+        arrOfStringNums.push(number.toString());
+    }
+    
+    for (const stringNum of arrOfStringNums) {
+        let splitNum = stringNum.split("")
+        let reversedNum = splitNum.reverse()
+        if (stringNum === reversedNum.join("")) {
+           console.log("true");
+           continue;
+        }
+        console.log("false");
+    }
     
 }
 
